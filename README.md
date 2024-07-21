2eb4fc8d2bf574e1e30d3f3eece9d253bd00bd9c
Jooyon Won <jyon1204@gmail.com>
2024-07-20 17:56:21 -0700

    DB Tutorial Complete

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Vintage Jazz Records Go/Gin Tutorial
 https://go.dev/doc/tutorial/web-service-gin
+
+
+# DB Setup:
+
+```
+$ psql -U postgres
+```
+
+```
+$ CREATE USER myuser WITH PASSWORD 'mypassword';
+```
+
+```
+$ CREATE DATABASE mydb;
+```
+
+```
+$ GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
+```
+
+Navigate into specific DB
+```
+$ \c vintagejazzrecord
+```
+
+Check Table Existence
+```
+$ \d+ albums
+```
+
+Drop Table
+```
+$ DROP TABLE IF EXISTS albums;
+```
--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/jmoiron/sqlx"
+
+	_ "github.com/lib/pq"
 )
 
+// album represents data about a record album.
+type album struct {
+	ID     int     `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var db *sqlx.DB
+
 func main() {
+	var err error
+	db, err = sqlx.Connect("postgres", "user=adminjazz dbname=vintagejazzrecord sslmode=disable password=mys3cret")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createTable()
+
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
@@ -15,22 +38,37 @@ func main() {
 	router.Run("localhost:8080")
 }
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
+// createTable creates the albums table if it does not exist
+func createTable() {
+	// Drop the table if it exists
+	// dropTableQuery := `DROP TABLE IF EXISTS albums;`
+	// _, err := db.Exec(dropTableQuery)
+	// if err != nil {
+	// 	log.Fatalf("Failed to drop table: %v", err)
+	// }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	createTableQuery := `CREATE TABLE IF NOT EXISTS albums (
+		id SERIAL PRIMARY KEY,
+		title varchar(50) NOT NULL,
+		artist varchar(50) NOT NULL,
+		price NUMERIC(10,2) NOT NULL
+	);`
+	_, err := db.Exec(createTableQuery)
+	if err != nil {
+		log.Fatalf("Failed to create table: %v", err)
+	} else {
+		log.Println("Table 'albums' is ready.")
+	}
 }
 
 // getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
+	var albums []album
+	err := db.Select(&albums, "SELECT * FROM albums")
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "could not retrieve albums"})
+		return
+	}
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
@@ -39,28 +77,32 @@ func getAlbums(c *gin.Context) {
 func getAlbumByID(c *gin.Context) {
 	id := c.Param("id")
 
-	// Loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
+	var a album
+	err := db.Get(&a, "SELECT * FROM albums WHERE id=$1", id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	c.IndentedJSON(http.StatusOK, a)
 }
 
 // postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
 	if err := c.BindJSON(&newAlbum); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid input"})
 		return
 	}
 
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
+	query := `INSERT INTO albums (title, artist, price) VALUES ($1, $2, $3) RETURNING id`
+	err := db.Get(&newAlbum.ID, query, newAlbum.Title, newAlbum.Artist, newAlbum.Price)
+	if err != nil {
+		// Print the actual error
+		log.Printf("Error inserting album: %v", err)
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "could not add album"})
+		return
+	}
+
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
